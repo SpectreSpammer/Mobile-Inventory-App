e3c148bc96e012a6c3401cd5e9215858486a6bb0
Kevin Baguisa <kevinbaguisa@outlook.com>
2018-08-16 13:14:57 +0800

    added some code for additemactivity and inventoryactivity

--- app/src/main/java/com/example/raynand/mobileinventoryapp/InventoryActivity.java
+++ app/src/main/java/com/example/raynand/mobileinventoryapp/InventoryActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 public class InventoryActivity extends AppCompatActivity {
 
@@ -11,9 +12,13 @@ public class InventoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        ListView inventoryList = findViewById(R.id.itemList);
+
     }
 
     public void addButtonClick(View v){
-        Intent addItemIntent = new Intent();
+        Intent addItemIntent = new Intent(InventoryActivity.this, AddItemActivity.class);
+        startActivity(addItemIntent);
     }
 }
