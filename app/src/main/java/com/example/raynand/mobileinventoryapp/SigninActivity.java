1beadeaa399de11367386c842e6547fecf6eaaac
raynand <raynandnavarra07@gmail.com>
2018-08-17 11:15:05 +0800

    Adding codes for SigninActivity ,SignupActivity

--- app/src/main/java/com/example/raynand/mobileinventoryapp/SigninActivity.java
+++ app/src/main/java/com/example/raynand/mobileinventoryapp/SigninActivity.java
@@ -1,13 +1,49 @@
 package com.example.raynand.mobileinventoryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SigninActivity extends AppCompatActivity {
+
+
+    private Button Signin;
+    private Button Back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        Signin = (Button)findViewById(R.id.btnSigin);
+        Back = (Button)findViewById(R.id.btnBack);
+
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenSignin();
+            }
+        });
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenHome();
+            }
+        });
+    }
+
+    public void OpenSignin()
+    {
+        Intent intent = new Intent(this, InventoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void OpenHome()
+    {
+        Intent intent = new Intent(this, StartPageActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/raynand/mobileinventoryapp/SignupActivity.java
+++ app/src/main/java/com/example/raynand/mobileinventoryapp/SignupActivity.java
@@ -1,13 +1,49 @@
 package com.example.raynand.mobileinventoryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private Button Signup;
+    private Button Back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Signup = (Button)findViewById(R.id.btnSignup);
+        Back = (Button)findViewById(R.id.btnBack);
+
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenSignin();
+            }
+        });
+
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenHome();
+            }
+        });
+    }
+
+    public void OpenSignin()
+    {
+        Intent intent = new Intent(this, SigninActivity.class);
+        startActivity(intent);
+    }
+
+    public void OpenHome()
+    {
+        Intent intent = new Intent(this, StartPageActivity.class);
+        startActivity(intent);
     }
 }
